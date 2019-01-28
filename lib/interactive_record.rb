e747d837dfe6dc5c75ed102d06fa0119061d7c05
Jessica Gladstone <jcg321@comcast.net>
2019-01-28 20:12:32 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -52,6 +52,8 @@ class InteractiveRecord
     
     self.column_names.each do |col_name|
       values <<  "'#{send(col_name)}'" unless send(col_name).nil?
- end
+    end
     values.join(", ")
+ end
+    
 end
