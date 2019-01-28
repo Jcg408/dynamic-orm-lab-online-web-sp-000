17902c1ce81c278cc8546382a13767c91a665d98
Jessica Gladstone <jcg321@comcast.net>
2019-01-28 20:50:37 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -66,9 +66,8 @@ class InteractiveRecord
  
  def self.find_by (hash)
    hash.each do |key, value|
-     
-   sql = <<-SQL
-     SELECT * FROM #{self.table_name} WHERE hash = ?
+     sql = <<-SQL
+     SELECT * FROM #{self.table_name} WHERE #{key} = ?
    SQL
    
    DB[:conn].execute(sql)
