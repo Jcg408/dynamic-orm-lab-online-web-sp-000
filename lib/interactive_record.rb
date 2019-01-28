9abb871e72e16a17554862b0e4b980d347eb006e
Jessica Gladstone <jcg321@comcast.net>
2019-01-28 20:30:00 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -66,7 +66,7 @@ class InteractiveRecord
  
  def self.find_by (input)
    sql = <<-SQL
-   SELECT * FROM #{self.table_name} WHERE '#{self.column_names}' = input
+   SELECT * FROM #{self.table_name} WHERE '#{self.column_names}' ==input
    SQL
    
    DB[:conn].execute(sql)
