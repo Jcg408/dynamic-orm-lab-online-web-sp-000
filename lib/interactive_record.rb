ad0cb0e43346e734fe0e3cf6ed2f21a4f02eda01
Jessica Gladstone <jcg321@comcast.net>
2019-01-28 19:53:45 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -16,10 +16,12 @@ class InteractiveRecord
 
     table_info = DB[:conn].execute(sql)
     column_names = []
+    
     table_info.each do |row|
       column_names << row["name"]
     end
     column_names.compact
   end
-  end
+  
+ 
 end
